e28934cd01087879551049ee64846a948813a9b4
dongdong <sgryan771211@gmail.com>
2019-04-30 17:50:34 +0800

    Add user login feature using passport and express-session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
 mongoose.connect('mongodb://localhost/expenseTracker', { useNewUrlParser: true })
 
@@ -13,6 +15,20 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'rsjdtkuyliuflif'
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+// 載入 Passport config
+require('./config/passport')(passport)
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 // 連線異常
 db.on('error', () => {
