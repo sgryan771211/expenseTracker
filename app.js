1ad98dcf80e6330f917129aa633adf0b69b65788
dongdong <sgryan771211@gmail.com>
2019-05-01 15:23:13 +0800

    Add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ require('./config/passport')(passport)
 
 app.use((req, res, next) => {
   res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
   next()
