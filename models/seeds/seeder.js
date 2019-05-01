f69b19d6f5655f0b977afb2ff5a89761990fa067
dongdong <sgryan771211@gmail.com>
2019-05-01 15:18:41 +0800

    Update seeder

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 const Record = require('../record')
+const bcrypt = require('bcryptjs')
+const User = require('../user')
 
-mongoose.connect('mongodb://localhost/expenseTracker', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/expenseTracker', { useNewUrlParser: true, useCreateIndex: true })
 
 const db = mongoose.connection
 
@@ -12,11 +14,52 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('db connected!')
 
-  Record.create({
-    name: '看電影',
-    category: '娛樂',
-    date: '2019-04-01',
-    amount: '100',
+  const testUser = User({
+    name: 'testuser',
+    email: 'testuser@gmail.com',
+    password: bcrypt.hashSync('123456', 10),
   })
+
+  testUser.save(err => {
+    if (err) return console.error(err)
+  })
+
+  Record.insertMany([
+    {
+      name: '午餐',
+      category: 'utensils',
+      date: '2019-04-01',
+      amount: '100',
+      userId: testUser._id,
+    },
+    {
+      name: '晚餐',
+      category: 'utensils',
+      date: '2019-04-01',
+      amount: '100',
+      userId: testUser._id,
+    },
+    {
+      name: '捷運',
+      category: 'shuttle-van',
+      date: '2019-04-01',
+      amount: '100',
+      userId: testUser._id,
+    },
+    {
+      name: '電影',
+      category: 'grin-beam',
+      date: '2019-04-01',
+      amount: '100',
+      userId: testUser._id,
+    },
+    {
+      name: '租金',
+      category: 'home',
+      date: '2019-04-01',
+      amount: '100',
+      userId: testUser._id,
+    },
+  ])
 
 })
